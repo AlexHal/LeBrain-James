3863f20f6dd60758c98af923a058cadfe6002b7c
Alex Hal <jonathan.halimi@mail.mcgill.ca>
2026-02-21 10:59:30 -0500

    new pass

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 #include <Controller.h>
 
-Controller controller ("RobotAP", "James000") ;
+Controller controller("RobotAP", "James000") ;
 
 void setup () {
-  Serial.begin (115200) ;
-  controller.configureL298N (9 , 7 , 6 , 10 , 5 , 4) ;
-  controller.setMotorMinPWM (90) ;
-  controller.setFailsafeTimeoutMs (1200) ;
-  controller.enableStatusLED( LED_BUILTIN ) ;
+  Serial.begin(115200) ;
+  controller.configureL298N(9, 7, 6, 10, 5, 4) ;
+  controller.setMotorMinPWM(90);
+  controller.setFailsafeTimeoutMs (1200);
+  controller.enableStatusLED(LED_BUILTIN);
 
   controller.beginAP(true) ;
   // Connect to WiFi " RobotAP " then open :
   // http ://10.0.0.2
 }
-void loop () {
+void loop() {
   controller.update ();
 }
