d6e59c5845ec2a411f8b4166331f2c9eff8e8ab5
Alex Hal <jonathan.halimi@mail.mcgill.ca>
2026-02-21 17:11:24 -0500

    new M3 code

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,64 @@
 Controller controller("LeBrain-James", "James000");
 
 //Thrower ie motor 3
-constexpr uint8_t M3_EN  = 3;
-constexpr uint8_t M3_IN1 = 2;
+constexpr uint8_t M3_EN  = 11;
+constexpr uint8_t M3_IN1 = 12;
 constexpr uint8_t M3_IN2 = 8;
 constexpr uint8_t M3_MIN_PWM = 90;
 
 bool   m3Enabled = false;
-int8_t m3Speed   = 0; 
+int8_t m3Speed  = 90; 
 
 
 
 //Setup motor 3
 void setMotorThree(int8_t spd) {
-    if (!m3Enabled || spd == 0) {
+    Serial.print("[M3] setMotorThree called: enabled=");
+    Serial.print(m3Enabled);
+    Serial.print(" spd=");
+    Serial.println(spd);
+
+    String in1, in2;
+
+
+    if (!m3Enabled) {
+      if (spd == 0) {
+        Serial.println("[M3] → BRAKE");
         digitalWrite(M3_IN1, HIGH);
         digitalWrite(M3_IN2, HIGH);
-        analogWrite(M3_EN, 0);  
+        in1 = "HIGH";
+        in2 = "HIGH";
         return;
+      } else if (spd > 0 ) {
+        digitalWrite(M3_IN1, HIGH);
+        digitalWrite(M3_IN2, LOW);
+        in1 = "HIGH";
+        in2 = "LOW";
+      } else {
+        digitalWrite(M3_IN1, LOW);
+        digitalWrite(M3_IN2, HIGH);
+        in1 = "LOW";
+        in2 = "HIGH";
+
+        spd = -spd;
+      }
     }
-    if (spd > 0) { digitalWrite(M3_IN1, HIGH); digitalWrite(M3_IN2, LOW); }
-    else         { digitalWrite(M3_IN1, LOW);  digitalWrite(M3_IN2, HIGH); spd = -spd; }
+        
 
     int pwm = map(constrain((int)spd, 0, 100), 0, 100, 0, 255);
     if (pwm > 0 && pwm < M3_MIN_PWM) pwm = M3_MIN_PWM;
+
+    Serial.print("[M3] → IN1=");
+    Serial.print(in1);
+    Serial.print(" IN2=");
+    Serial.print(in2);
+    Serial.print(" pwd=");
+    Serial.print(pwm);
+    
+
     analogWrite(M3_EN, pwm);
+
+    
 }
 
 //Togle M3
@@ -35,10 +69,6 @@ void toggleM3() {
     m3Enabled = !m3Enabled;
     Serial.print("[M3] "); Serial.println(m3Enabled ? "ENABLED" : "DISABLED");
     setMotorThree(m3Speed);  
-
-void onDrive(int8_t left, int8_t right) {
-    m3Speed = (int8_t)(((int)left + (int)right) / 2);
-    setMotorThree(m3Speed);
 }
 
 
@@ -51,7 +81,7 @@ void setup () {
   controller.setFailsafeTimeoutMs (1200);
   controller.enableStatusLED(LED_BUILTIN);
 
-  controller.registerDriveCallback(onDrive);
+  //controller.registerDriveCallback(onDrive);
   controller.registerButton("M3", toggleM3);  // appears on the web UI
 
 
@@ -60,6 +90,7 @@ void setup () {
   // Connect to WiFi " RobotAP " then open :
   // http ://10.0.0.2
 }
+
 void loop() {
   controller.update ();
 }
